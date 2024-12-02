fda43f0226a9f45ee8820480e385b700a8e95afa
godskyson <king03603@kakao.com>
2024-12-02 21:34:08 +0900

    Update 5_료이키텐카이 무량공처.py

--- pages/5_료이키텐카이 무량공처.py
+++ pages/5_료이키텐카이 무량공처.py
@@ -102,7 +102,7 @@ def show_recommendations(select):
                     star = "⭐"
                     star_black = " ★ "
                     lists = f" ∙  **{cls}** :gray[{prof}] \n {star*ratings}{star_black*(5-ratings)}"
-                    text = "asdfasdf\n asdfasdf
+                    text = "asdfasdf\n asdfasdf"
                     st.write(text)
 
 
