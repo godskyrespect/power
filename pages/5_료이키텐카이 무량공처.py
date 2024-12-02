e2375c4b1b116671ff81eac9ed886f506a0f2cad
godskyson <king03603@kakao.com>
2024-12-02 17:33:17 +0900

    Update 5_료이키텐카이 무량공처.py

--- pages/5_료이키텐카이 무량공처.py
+++ pages/5_료이키텐카이 무량공처.py
@@ -66,15 +66,19 @@ def show_recommendations(select):
     st.write(rec_reason)
     
     st.divider()
-
-    options = st.multiselect("과목 표시 선택", recs['recommendations])
+    
+    selection = [item['subject'] for item in recs['recommendations']]
+    options = ("표시하지 않을 교과를 선택해 주세요.", selection)
     for rec in recs['recommendations']:
         subject = rec['subject']
-        classes = rec['class']
-        with st.container(border=True):
-            st.header(f'{subject} 교과 추천')
-            for idx, cls in enumerate(classes):
-                st.write(f'{idx}. {cls}')
+        if subject in options:
+            continue
+        else:
+            classes = rec['class']
+            with st.container(border=True):
+                st.header(f'{subject} 교과 추천')
+                for idx, cls in enumerate(classes):
+                    st.write(f'{idx}. {cls}')
            
 st.title('우리 학교 수업')
 search_query = st.text_input('검색할 내용을 입력하세요:', placeholder='수업명을 입력하세요')
