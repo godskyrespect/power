55826944347207df4bc035e622e967ffd10e83b8
godskyson <king03603@kakao.com>
2024-12-02 22:28:43 +0900

    Update 5_4신천인데 수업 가는 걸 깜빡했더니 전설이 되어있었다.py

--- pages/5_4신천인데 수업 가는 걸 깜빡했더니 전설이 되어있었다.py
+++ pages/5_4신천인데 수업 가는 걸 깜빡했더니 전설이 되어있었다.py
@@ -115,7 +115,6 @@ tab1, tab2 = st.tabs(["수업 검색", "추천 수업"])
 with tab1:
     st.title("🔍 찾고싶은 수업을 검색해보세요.")
     search_query = st.text_input("search", placeholder='수업명을 입력하세요', label_visibility='hidden')
-    st.write('여기는 우리 학교 수업 페이지입니다.')
 
 with tab2:
     st.header('울학교 선배님들의 추천 ✨')
