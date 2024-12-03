66febd34abb9cf78ab84da6bcdb7413afe11e783
godskyson <king03603@kakao.com>
2024-12-03 20:08:17 +0900

    Update 5_4신천인데 수업 가는 걸 깜빡했더니 전설이 되어있었다.py

--- pages/5_4신천인데 수업 가는 걸 깜빡했더니 전설이 되어있었다.py
+++ pages/5_4신천인데 수업 가는 걸 깜빡했더니 전설이 되어있었다.py
@@ -113,7 +113,9 @@ def show_recommendations(select):
 
 
   
-st.title('우리 학교 수업')
+
+# st.title('우리 학교 수업')
+st.title("🕋ﷲ﷽")
 search_query = st.text_input("🔍 찾고싶은 수업을 검색해보세요.", placeholder='수업명을 입력하세요')
 tab1, tab2 = st.tabs(["수업 검색", "추천 수업"])
 with tab1:
