1f52673e7bc2769bac64a2751be9bdf63b56563f
godskyson <king03603@kakao.com>
2024-12-03 16:15:51 +0900

    Update 5_4신천인데 수업 가는 걸 깜빡했더니 전설이 되어있었다.py

--- pages/5_4신천인데 수업 가는 걸 깜빡했더니 전설이 되어있었다.py
+++ pages/5_4신천인데 수업 가는 걸 깜빡했더니 전설이 되어있었다.py
@@ -138,13 +138,13 @@ with tab2:
             show_recommendations(selection)
 
 
-if st.session_state.logged_in:
-    with st.sidebar:
-        st.write(f" {st.session_state.student_id}")
-        st.write(f" {st.session_state.name}")
-        if st.button("로그아웃"):
-            st.session_state.logged_in = False
-            st.session_state.student_id = ""
-            st.session_state.name = ""
-            st.success("로그아웃되었습니다.")
+# if st.session_state.logged_in:
+#     with st.sidebar:
+#         st.write(f" {st.session_state.student_id}")
+#         st.write(f" {st.session_state.name}")
+#         if st.button("로그아웃"):
+#             st.session_state.logged_in = False
+#             st.session_state.student_id = ""
+#             st.session_state.name = ""
+#             st.success("로그아웃되었습니다.")
 
