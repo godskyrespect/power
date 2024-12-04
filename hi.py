aa298c759d8158b759c9a5f9ee27810cb13cc4f4
godskyson <king03603@kakao.com>
2024-12-04 15:19:08 +0900

    Update hi.py

--- hi.py
+++ hi.py
@@ -5,11 +5,11 @@ if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
 if "username" not in st.session_state:
     st.session_state.username = ""
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
+    
+hide_github_icon = “”"
+
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+“”"
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 # 제목 설정
