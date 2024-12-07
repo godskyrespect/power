91ea97acf6e23ad958e1dec07e39c46e82f6fbc3
godskyson <king03603@kakao.com>
2024-12-07 21:52:40 +0900

    Update 5_4신천인데 수업 가는 걸 깜빡했더니 전설이 되어있었다.py

--- pages/5_4신천인데 수업 가는 걸 깜빡했더니 전설이 되어있었다.py
+++ pages/5_4신천인데 수업 가는 걸 깜빡했더니 전설이 되어있었다.py
@@ -119,7 +119,7 @@ tab1, tab2 = st.tabs(["수업 검색", "추천 수업"])
 with tab1:
     matches = [item for item in school_data if item.get("class_name") == search_query]
     if matches:
-        st.write(matches['class_name'])
+        st.write(matches)
     else:
         if search_query:
             st.error("검색된 강의가 없습니다", icon="❕")
