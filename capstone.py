1f4f9390f8f7f8be5a8a24c54a4b8bb23c8e5049
godskyson <king03603@kakao.com>
2024-12-10 02:02:42 +0900

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -6,3 +6,8 @@ def RequestApi(link):
   response = requests.get(api_url)
   json_data = response.json()
   return json_data
+
+def RequestPost(data):
+  api_url = "http://13.211.145.139:8000/school/upload"
+  response = requests.post(url, json=data)
+  return response.status_code
