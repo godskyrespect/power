7a0e59110d90d71fbc20dd3b5a57175abadda04d
godskyson <king03603@kakao.com>
2024-12-09 22:42:52 +0900

    Update 4_📚_나의 수업 평가.py

--- pages/4_📚_나의 수업 평가.py
+++ pages/4_📚_나의 수업 평가.py
@@ -1,31 +1,17 @@
+import config
 import streamlit as st
-from pymongo import MongoClient
 import pandas as pd
 from openai import OpenAI
 from panda import pandaChickenApi
-import config
-
 
 # OpenAI 연결 설정 ====================================
 client = OpenAI(api_key=st.secrets.OPENAI_API_KEY)
 
-## 2. API호출(수업정보, 리뷰정보)
-url_school = "http://13.211.145.139:8000/school"
-response_sl = requests.get(url_school)
+## 0. API호출(수업정보, 리뷰정보)
 collection = pandaChickenApi("school")
-
-url_reviews = "http://13.211.145.139:8000/school/reviews"
-response_rt = requests.get(url_reviews)
 classes_review_collection = pandaChickenApi("school/reviews")
-
-url_student = "http://13.211.145.139:8000/user/student"
-response_rt = requests.get(url_student)
 student_collection = pandaChickenApi("user/student")
-
-url_evaluation = "http://13.211.145.139:8000/teacher/evaluation"
-response_rt = requests.get(url_evaluation)
 evaluation_collection = pandaChickenApi("teacher/evaluation")
-
 
 ## 1. 작성된 프롬프트를 LLM에 전달하고 응답을 받는 함수(get: 프롬프트)
 def chatgpt_generate(query):
