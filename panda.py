a877502919ed450900a4cf9a55bd986a27a65461
godskyson <king03603@kakao.com>
2024-12-09 22:40:32 +0900

    Update and rename utils.py to panda.py

--- panda.py
+++ panda.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-def capstoneApi(link):
+def pandaChickenApi(link):
   api_url = f"http://13.211.145.139:8000/{link}"
   response = requests.get(api_url)
   json_data = response.json()
