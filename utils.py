ca13a48b489d70de55cdb895eb5965e9311fcf7d
godskyson <king03603@kakao.com>
2024-12-09 22:36:06 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-def request_api(link):
+def capstoneApi(link):
   api_url = f"http://13.211.145.139:8000/{link}"
-  response = requests.get(url_school)
+  response = requests.get(api_url)
   json_data = response.json()
   return json_data
