ec57b5adc87bb639cac46742f5a71dadd48dca7e
godskyson <king03603@kakao.com>
2024-12-10 15:53:55 +0900

    Update recommendation_batch.py

--- scripts/recommendation_batch.py
+++ scripts/recommendation_batch.py
@@ -7,11 +7,12 @@ from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 
 from utils import get_embeddings, cosine_similarity, call_openai
-#, call_openai
 
+# 1. 표시할 키워드 선정
 COMBINATIONS = ['열정적인 교수님', '유익한 수업', '도움되는 수업', '꿀강', '얻어가는 수업']
 KEYWORDS_CONTEXT = ['열정', '유익', '도움', '꿀강', '얻어']
 
+# 2. 키워드가 포함된 리뷰 추출
 def extract_keywords(review_text):
     keywords = []
 
@@ -20,7 +21,7 @@ def extract_keywords(review_text):
             keywords.append(word)
     return keywords
 
-
+# 3. MongoDB에서 학과 리뷰 데이터 불러오기
 def fetch_subject_info():
     load_dotenv()
     username = urllib.parse.quote_plus(os.environ['MONGODB_USERNAME'])
@@ -34,7 +35,7 @@ def fetch_subject_info():
     print("^^ : MongoDB에서 수업 정보를 불러오는데 성공했습니다.")
     return classes_info
 
-
+# 4. 리뷰에서 추출한 키워드를 바탕으로 수업에 포함된 키워드로 묶기
 def create_candidates(class_infos):
     candidates = []
     for idx, info in enumerate(class_infos):
@@ -57,6 +58,7 @@ def create_candidates(class_infos):
     return candidates
 
 
+# 5. 키워드와 수업과 연관지은 키워드 임베딩, 유사도 측정
 def create_recommendations(query, candidates):
     contexts = [cand["keywords"] for cand in candidates]
     query_embedding = get_embeddings([query], model='text-embedding-3-large')[0]
@@ -85,7 +87,8 @@ def create_recommendations(query, candidates):
             final_recommendations[rec['subject']].append(class_name)
     print("^^ : 키워드와 유사도가 높은 순으로 정렬하는데 성공했습니다.")
     return final_recommendations
-    
+
+# 6. 프롬프트 작성, LLM을 이용한 추천사유 생성
 def create_recommmendation_text(query, recommendations):
     prompt = f"""당신은 학교 강의평 사이트에서 강의평가 텍스트를 바탕으로 메뉴를 추천해주는 내타입수업AI입니다.
 아래 목록은 {query}와 관련된 수업을 연관성 높은 순서로 나열한 목록입니다.
@@ -108,7 +111,7 @@ def create_recommmendation_text(query, recommendations):
     print("^^ : GPT를 사용하여 추천사유를 생성하는데 성공하였습니다.")
     return recommendation_message
 
-
+# 7. DB에 추천사유와 추천과목 업로드
 def insert_to_mongo(query, text, recommendations):
     load_dotenv()
     username = urllib.parse.quote_plus(os.environ['MONGODB_USERNAME'])
@@ -129,6 +132,7 @@ def insert_to_mongo(query, text, recommendations):
     
     return result
 
+## ============== 리뷰 추천 코드 ============== ##
 def recommend_batch():
     infos = fetch_subject_info()
     candidates = create_candidates(infos)
