58a8d4758c72a93d3403cb70adab9032826d126c
godskyson <king03603@kakao.com>
2025-07-10 16:19:39 +0900

    Update serial.py

--- pages/serial.py
+++ pages/serial.py
@@ -14,7 +14,25 @@ port = st.text_input("포트", "/dev/ttyACM0")
 fqbn = st.text_input("보드 FQBN", "arduino:avr:uno")
 st.session_state.agent_id = st.text_input("Agent ID",placeholder="ASTDIO-")
 serial_on = st.toggle("시리얼 통신 영역 전개")
-if serial_on:
-  st.write("시리얼 통신 시작")
+
 st.write("현재 통신 AGENT ID : ", st.session_state.agent_id)
 st.divider()
+
+serial_area = st.empty()
+
+if serial_on:
+  st.write("시리얼 통신 시작")
+  if not st.session_state.agent_id:
+    st.warning("Client와 연결되어 있지 않음.")
+  else:
+      while True:
+          try:
+              res = requests.get(f"http://{SERVER_HTTP}/get_serial", params={"agent_id": st.session_state.agent_id})
+              if res.status_code == 200:
+                  data = res.json()["serial_data"]
+                  if data:
+                      serial_area.text('\n'.join(data))
+              time.sleep(1)
+          except Exception as e:
+              st.error(f"에러: {e}")
+              time.sleep(5)
